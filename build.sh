b444d09c0e122f5a81ed68b8126c6e98db8214d2
Marcelo <marojas13@live.com.ar>
2026-04-18 21:10:30 -0600

    Add PHP runtime config for Render

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Install composer if not available
+if ! command -v composer &> /dev/null; then
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+fi
+
 composer install --optimize-autoloader --no-dev
 
 php artisan migrate --force
